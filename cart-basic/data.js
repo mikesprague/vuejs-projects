08ed0015d39b6a89962919f27701edbeb28253b9
Mike Sprague <mikesprague@gmail.com>
2016-08-20 22:03:49 -0400

    Add first iteration of cart

---
+++ cart-basic/data.js
@@ -0,0 +1,64 @@
+var inventory = [{
+  'name': 'USB Stick',
+
+  'options': [{
+      'id': 1,
+      'details': '16GB',
+      'price': 6.95
+  }, {
+      'id': 2,
+      'details': '32GB',
+      'price': 12.95
+  }, {
+      'id': 3,
+      'details': '64GB',
+      'price': 25.95
+  }]
+}, {
+  'name': 'USB Plug',
+
+  'options': [{
+      'id': 4,
+      'price': 4.35,
+      'details': '3ft cable'
+  }]
+}, {
+  'name': 'Small Phone',
+
+  'options': [{
+      'id': 5,
+      'price': 4.35,
+      'details': 'Nokia Phone'
+  }]
+}, {
+  'name': 'Camera',
+
+  'options': [{
+      'id': 6,
+      'price': 76.50,
+      'details': 'Blue Camera'
+  }, {
+      'id': 7,
+      'price': 86.50,
+      'details': 'Red Camera'
+  }, {
+      'id': 8,
+      'price': 55.50,
+      'details': 'Yellow Camera'
+  }, {
+      'id': 9,
+      'price': 45.50,
+      'details': 'Purple Camera'
+  }]
+}];
+var incart = [{
+  price: 100,
+  listitem: 'First Test Product',
+  quantity: 4,
+  id: 1
+}, {
+  price: 10,
+  listitem: 'Another Test Product',
+  quantity: 2,
+  id: 2
+}];
